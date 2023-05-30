2c6b4fb81f154d2ef64c49adfcee7d3634233010
hjl210 <58891426+hjl210@users.noreply.github.com>
2023-05-30 11:10:13 +0800

    Add files via upload

---
+++ shucheng2/vue.config.js
@@ -0,0 +1,35 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave:false,
+  dev:{
+	assetsSubDirectory:'static',
+	assetsPublicPath:'./'
+  },
+  devServer:{
+  	  proxy:{
+  		  '/shu':{
+  			  target:'https://api.zhuishushenqi.com',
+  		      changOrigin:true,
+			  secure:true,
+			  pathRewrite:{
+				  '^/shu':'/'
+			  }
+  		  },
+		  '/www':{
+		      target:'https://www.zhuishushenqi.com',
+		      changOrigin:true,
+		  	  pathRewrite:{
+		  	     '^/www':'/'
+		  	  }
+		  },
+		  '/http':{
+		      target:'http://111.229.37.167',
+		      changOrigin:true,
+			  pathRewrite:{
+			     '^/http':'/'
+			  }
+		  }
+  	  }
+  }
+})
